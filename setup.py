70f314eff700f9cc2c0eb17af0935adb01fb9627
unknown <legendashish66@gmail.com>
2023-04-13 10:38:38 +0545

    minor changes in setup file

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: Apache Software License v2.0',   # Again, pick a license
+    'License :: OSI Approved :: Apache Software License',   
     'Programming Language :: Python :: 3.9',
   ],
 )
