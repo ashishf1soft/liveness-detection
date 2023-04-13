e49da5931eae6908ce4f7b6e85be1df8e7214777
unknown <legendashish66@gmail.com>
2023-04-13 11:43:00 +0545

    minor changes in setup file

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 setup(
-  name = 'liveness',         
+  name = 'image-liveness',         
   packages = ['Liveness'],  
-  version = '0.1',      
+  version = '1.2',      
   license='Apache License 2.0',        
   description = 'This library offers a liveness detection on static images, which can be used to distinguish if it is a live capture or a spoofed image based on facial features and other indicators of authenticity.',  
   author = 'Ashish Thapa',                   
@@ -12,7 +12,7 @@ setup(
   keywords = ['LIVENESS','FACE FACE','DETECTION'], 
   install_requires=[            # I get to this in a second
           'numpy',
-          'cv2',
+          'opencv-python',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
